c3aa75c78a72082e04501dc4292455b38e63131e
kryffin <nicolaskleinhentz@gmail.com>
2021-09-30 16:38:39 +0200

    renamed some files

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class DijsktraRiver : MonoBehaviour
+public class PathFinder : MonoBehaviour
 {
 
     public int width, height; //graph size
